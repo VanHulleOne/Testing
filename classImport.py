ff227d11951b7e694d0e889a4fe8ca8d7214dec5
Luke Home PC <beardedone85@gmail.com>
2016-05-15 20:27:59 -0500

    Making cool stuff

---
+++ classImport.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 14 13:11:34 2016
+
+@author: Luke
+"""
+
+from getAttr import codes
--- getAttr.py
+++ getAttr.py
@@ -6,7 +6,6 @@ Created on Fri May 13 13:57:47 2016
 """
 
 from functools import wraps
-from classImport import c1
 
 m_dict = {}
 g_dict = {}
@@ -19,10 +18,9 @@ def grouper(mode_dict=None, group=None, modal=True, name=None):
                 funcName = func.__name__
             else:
                 funcName = name
-            if mode_dict[group] == funcName:#code:
-                active = True
-            else:
-                active = False
+            
+            active = mode_dict[group] == funcName
+
             if modal:
                 mode_dict[group] = funcName#code
             else:
@@ -36,64 +34,61 @@ def gcode(**kwargs):
     
 def mcode(group=None, modal=None):
     return grouper(m_dict, group=group, modal=modal)
-   
-@gcode(group=1)
-def g0(val, active=False):
-    if active:
-        return 'Active ' + str(val)
-    return 'Not Active ' + str(val)
-    
-@gcode(group=1)
-def g1(val, active=False):
-    if active:
-        return 'Active ' + str(val)
-    return 'Not Active ' + str(val)
-    
-@gcode(group=1, modal=False)
-def g2(val, active=False):
-    if active:
-        return 'Not good'
-    return 'Working arc'
-
-@gcode(group=2)
-def g91(active=False):
-    if active:
-        return 'Active ' + str(91)
-    return 'Not Active ' + str(91)
-    
-@gcode(group=2)
-def g90(active=False):
-    if active:
-        return 'Active ' + str(90)
-    return 'Not Active ' + str(90)
-
-@mcode(group=1)
-def m6(_, active=False):
-    if active:
-        return 'Already Changing'
-    return 'Start Changing'
-
-@mcode(group=1)    
-def m5(active=False):
-    if active:
-        return 'Trying'
-    return 'Stopping'
 
 class tester(object):
+   
+    @gcode(group=1)
+    def g0(self, val, active=False):
+        if active:
+            return 'Active ' + str(val)
+        return 'Not Active ' + str(val)
+        
+    @gcode(group=1)
+    def g1(self, val, active=False):
+        if active:
+            return 'Active ' + str(val)
+        return 'Not Active ' + str(val)
+        
+    @gcode(group=1, modal=False)
+    def g2(self, val, active=False):
+        if active:
+            return 'Not good'
+        return 'Working arc'
+    
+    @gcode(group=2)
+    def g91(self, active=False):
+        if active:
+            return 'Active ' + str(91)
+        return 'Not Active ' + str(91)
+        
+    @gcode(group=2)
+    def g90(self, active=False):
+        if active:
+            return 'Active ' + str(90)
+        return 'Not Active ' + str(90)
+    
+    @mcode(group=1)
+    def m6(self, active=False):
+        if active:
+            return 'Already Changing'
+        return 'Start Changing'
+    
+    @mcode(group=1)    
+    def m5(self, active=False):
+        if active:
+            return 'Trying'
+        return 'Stopping'
     
     def __init__(self):
         self.codes = {'g93': 1, 'g94': 1, 'x':4}
         for code, group in self.codes.iteritems():
             self.__dict__[code] = self.over(code, group)
-        
-    def x(self):
-        print 'x'
     
     def over(self, code, group):
         @gcode(group=group, name=code)
         def generic(active=False):
             print 'Is Active: ' + str(active)
-#            print code
-            return code #self.codes[code]
-#        print 'Leaving over'
+            return code
         return generic
+
+codes = tester()
